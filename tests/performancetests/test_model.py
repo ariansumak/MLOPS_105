977b1fa4ef8ef507bd5ecb6f9e8f09e8f30381e2
rokojakasa <roko.jakasa@gmail.com>
2026-01-21 18:31:58 +0100

    fixed workflow

--- tests/performancetests/test_model.py
+++ tests/performancetests/test_model.py
@@ -2,8 +2,7 @@ import wandb
 import os
 import time
 import torch
-from my_project.models import MyModel
-
+from pneumoniaclassifier.model import _build_model
 logdir="tmp_model"
 
 def load_model(model_name):
@@ -14,10 +13,14 @@ def load_model(model_name):
     artifact = api.artifact(model_name)
     artifact.download(root=logdir)
     file_name = artifact.files()[0].name
-    return MyModel.load_from_checkpoint(f"{logdir}/{file_name}")
+    model = _build_model(model_name="efficientnet_b0", num_classes=2, pretrained=True)
+    state_dict = torch.load(os.path.join(logdir, file_name), map_location="cpu", weights_only=True)
+    model.load_state_dict(state_dict)
+    return model
 
 def test_model_speed():
     model = load_model(os.getenv("MODEL_NAME"))
+    model.eval()
     start = time.time()
     for _ in range(100):
         model(torch.rand(1, 1, 28, 28))
